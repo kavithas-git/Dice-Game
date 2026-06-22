6f31f3fb462b4ced0dea559cc126057cc257a552
kavithas-git <kavitha850109@gmail.com>
2026-06-22 22:26:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", "images/" + randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+document.querySelector(".img2").setAttribute("src", "images/" + randomDiceImage2);
+
+if(randomNumber1 > randomNumber2) { 
+
+     document.querySelector("h1").innerHTML = "Player1 Wins!";
+
+} else if (randomNumber2 > randomNumber1) {
+
+    document.querySelector("h1").innerHTML = "Player2 Wins!";
+
+}else {
+
+    document.querySelector("h1").innerHTML = "Draw";
+
+}
+
